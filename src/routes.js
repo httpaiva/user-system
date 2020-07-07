d5dd8a38c226961472ca2a098bcc95fda4ae02f8
gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>
2020-07-07 17:18:15 -0300

    #5 Creating routes for password recovery.
    Signed-off-by: gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>
    Signed-off-by: murilogds <murilo.goomes@hotmail.com>
    Signed-off-by: eliseukadesh67 <eliseu.kadesh1967@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -8,11 +8,14 @@ const authMiddleware = require('./utils/authMiddleware');
 //Controllers:
 const UserController = require('./controllers/UserController');
 const SessionController = require('./controllers/SessionController');
+const PasswordController = require('./controllers/PasswordController');
 
 //Routes:
 routes.post('/user', UserController.create );
 routes.get('/users', authMiddleware ,UserController.index);
 routes.get('/login', SessionController.login);
+routes.post('/forgotpassword', PasswordController.requestNewPassword);
+routes.put('/forgotpassword', PasswordController.createNewPassword);
 
 
 //Export:
